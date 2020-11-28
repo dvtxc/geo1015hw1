ab5e11f8749e17ce7b09d1166242794f50818ae8
dvtxc <dmitri95dwt@gmail.com>
2020-11-28 11:28:50 +0100

    added by daniel

--- my_code_hw01.py
+++ my_code_hw01.py
@@ -1,7 +1,7 @@
 #-- my_code_hw01.py
 #-- hw01 GEO1015.2020
-#-- [YOUR NAME]
-#-- [YOUR STUDENT NUMBER] 
+#-- DANIEL DOBSON
+#-- 5152739
 #-- [YOUR NAME]
 #-- [YOUR STUDENT NUMBER] 
 
@@ -13,7 +13,47 @@ import scipy.spatial
 import startin 
 #-----
 
+def bbox(list_pts_3d):
+    '''
+    BOUNDINGBOX
+    This function creates a bounding box. 
+    Input: list of x, y, z points.
+    Output: bbox (min_x, min_y, max_x, max_y)
+    '''
+    x_pts = [i[0] for i in list_pts_3d]
+    y_pts = [i[1] for i in list_pts_3d]
+        
+    min_x, min_y, max_x, max_y = min(x_pts), min(y_pts), max(x_pts), max(y_pts)
+    bbox = min_x, min_y, max_x, max_y
+    return bbox
 
+def raster(list_pts_3d, jparams):
+    '''
+    RASTER
+    Input: list of points x, y, z
+    Output: [[x1,y1], [x2,y2], ...., [xn, yn]]
+    '''
+    #Prepare points for raster
+    x_pts = [i[0] for i in list_pts_3d]
+    y_pts = [i[1] for i in list_pts_3d]
+    z_pts = [i[2] for i in list_pts_3d]
+    x_pts.sort()
+    y_pts.sort()
+    #Bbox for raster input, extracting max_x, max_y
+    bbox_raster = bbox(list_pts_3d)
+    max_x, max_y = bbox_raster[2], bbox_raster[3]
+    ###Make raster
+    #Making x, y *center* cells for raster.
+    xcells = [(i+jparams["cellsize"]/2) for i in x_pts]
+    xcells = [cell for cell in xcells if cell < max_x]
+    ycells = [(i+jparams["cellsize"]/2) for i in y_pts]
+    ycells = [cell for cell in ycells if cell < max_y]
+    #Putting x,y cells together to form raster
+    raster = []
+    for i in xcells:
+        for j in ycells:
+            raster.append([i,j])
+    return raster 
 
 def nn_interpolation(list_pts_3d, j_nn):
     """
@@ -28,16 +68,50 @@ def nn_interpolation(list_pts_3d, j_nn):
         returns the value of the area
  
     """  
-    # print("cellsize:", j_nn['cellsize'])
+    ### This is what the inputs look like
+    #print(list_pts_3d)
+    #print(j_nn)
+    #print(list_pts_3d[0][0])
+    #for i in list_pts_3d:
+        #print(i[0])
+    
+    x_pts = [i[0] for i in list_pts_3d]
+    y_pts = [i[1] for i in list_pts_3d]
+    z_pts = [i[2] for i in list_pts_3d]
+    x_pts.sort()
+    y_pts.sort()
+    
+    bbox_nn = bbox(list_pts_3d)
+    print(bbox_nn)
+    min_x, min_y, max_x, max_y = bbox_nn[0], bbox_nn[1], bbox_nn[2], bbox_nn[3] 
+    
+    ### Convert list of points to array for preprocessing the input data
+    #list_pts = numpy.array(list_pts_3d)
+    
+    #Lower left corner
+    xll, yll = min_x, min_y
+    ll = xll, yll
+    print('LOWERLEFTCORNER: {}'.format(ll))
 
+    #Center lower left corner, center upper right corner
+    cll_x, cll_y  = (xll + j_nn["cellsize"]/2, yll + j_nn["cellsize"]/2)
+    cur_x, cur_y = (max_x - j_nn["cellsize"]/2, max_y - j_nn["cellsize"]/2)
+    print('cll_x', cll_x, 'cll_y', cll_y)
+    print('cur_x', cur_x, 'cur_y', cur_y)
+    
+    raster_nn = raster(list_pts_3d, j_nn)
+    #print('raster', raster(list_pts_3d,j_nn))
+
+     
     #-- to speed up the nearest neighbour us a kd-tree
     # https://docs.scipy.org/doc/scipy/reference/generated/scipy.spatial.KDTree.html#scipy.spatial.KDTree
     # https://docs.scipy.org/doc/scipy/reference/generated/scipy.spatial.KDTree.query.html#scipy.spatial.KDTree.query
-    # kd = scipy.spatial.KDTree(list_pts)
-    # d, i = kd.query(p, k=1)
-
+    #kd = scipy.spatial.KDTree(list_pts)
+    #print(kd)
+    #d, i = kd.query(kd, k=1)
+    #print(d, i)
+    
     print("File written to", j_nn['output-file'])
-
 
 
 def idw_interpolation(list_pts_3d, j_idw):
